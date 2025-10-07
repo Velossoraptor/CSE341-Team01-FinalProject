4c4142d2f3f2486a78fa8c1f671def80fa60620a
velossoraptor <taliamolsen@gmail.com>
2025-10-07 10:28:14 -0400

    Added Delete User and downgrade role - NOT TESTED BECAUSE AUTH DOESNT WORK ON LOCALHOST

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { upgradeToAdmin, Allusers } = require('../controllers/users.controller');
+const { upgradeToAdmin, Allusers, deleteUser, downgradeRole } = require('../controllers/users.controller');
 const { verifyGoogleToken } = require('../middleware/VerifyGoogleToken');
+const { authorizeAdminOnly } = require('../middleware/auth.js');
 
 router.get('/', Allusers);
 router.post('/upgrade-role', verifyGoogleToken, upgradeToAdmin);
+router.delete('/:id', verifyGoogleToken, authorizeAdminOnly, deleteUser);
+router.put('/downgrade-role', verifyGoogleToken, authorizeAdminOnly, downgradeRole);
 
 module.exports = router;
